207cbf7bf97c47138f8d14057b384de71800e774
Samandareshmamatov <samandareshmamatov1127@gmail.com>
2023-03-05 16:14:55 +0500

    Change Weekly

--- src/pages/Weekly/Weekly.jsx
+++ src/pages/Weekly/Weekly.jsx
@@ -37,7 +37,7 @@ const Weekly = ({ location }) => {
           </>
         )}
         <Link className="weekly-link" to="/">
-          Home
+          Home Page
         </Link>
       </div>
     </div>
